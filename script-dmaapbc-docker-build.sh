d2d105fceceff16c95b73f1fb2dbf4bd51a183d1
Lusheng <lji@research.att.com>
2017-03-23 19:29:13 +0000

    fixed docker script
    Change-Id: Ia9b05e0027f74455db84ebf781ac050689a23e06
    Signed-off-by: Lusheng <lji@research.att.com>

--- script-dmaapbc-docker-build.sh
+++ script-dmaapbc-docker-build.sh
@@ -43,7 +43,7 @@ VERSION2=$(echo "$VERSION" | cut -f1-2 -d'.')
 
 TIMESTAMP=$(date +%C%y%m%dT%H%M%S)
 LFQI="${IMAGE}:${VERSION}${TIMESTAMP}"
-BUILD_PATH="${WORKSPACE}/target/stage"
+BUILD_PATH="${WORKSPACE}"
 # build a docker image
 docker build --rm -f "${WORKSPACE}"/Dockerfile -t "${LFQI}" "${BUILD_PATH}"
 
